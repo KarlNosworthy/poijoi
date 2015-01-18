94b51c714c1778519806c528272304b440c1f7a8
Karl Nosworthy <k.nosworthy@gmail.com>
2015-01-18 20:37:12 +0000

    Added SQLOutputStreamWriter and tests.

---
+++ core/src/main/java/com/karlnosworthy/poijoi/io/writer/sql/SQLOutputStreamWriter.java
@@ -0,0 +1,79 @@
+package com.karlnosworthy.poijoi.io.writer.sql;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.List;
+
+import com.karlnosworthy.poijoi.io.writer.Writer;
+import com.karlnosworthy.poijoi.jdbc.SQLStatementCreator;
+import com.karlnosworthy.poijoi.model.PoijoiMetaData;
+import com.karlnosworthy.poijoi.model.TableDefinition;
+
+public class SQLOutputStreamWriter implements Writer<OutputStream> {
+
+	@Override
+	public boolean write(OutputStream output, PoijoiMetaData metaData, WriteType writeType) throws Exception {
+		
+		if (isValidOutput(output) && isValidMetadata(metaData)) {
+			
+			SQLStatementCreator sqlStatementWriter = new SQLStatementCreator();
+			
+			StringBuffer buffer = new StringBuffer();
+			
+			if (writeType != WriteType.DATA_ONLY) {
+				for (String tableName : metaData.getTableDefinitions().keySet()) {
+					TableDefinition tableDefinition = metaData.getTableDefinition(tableName);
+					buffer.append(sqlStatementWriter.buildCreateTableSQL(tableDefinition));
+					buffer.append("\n");
+				}
+			}
+			
+			if (writeType != WriteType.SCHEMA_ONLY) {
+				for (String tableName : metaData.getTableDefinitions().keySet()) {
+					TableDefinition tableDefinition = metaData.getTableDefinition(tableName);
+					List<HashMap<String, Object>> tableData = metaData.getTableData(tableName);
+					
+					List<String> sqlInserts = sqlStatementWriter.buildInsertTableSQL(tableName, tableData, tableDefinition.getColumnDefinitions());				
+					
+					for (String sqlInsert : sqlInserts) {
+						buffer.append(sqlInsert);
+						buffer.append("\n");
+					}
+				}
+			}
+			
+			output.write(buffer.toString().getBytes());
+			return true;
+		}
+		return false;
+	}
+	
+	boolean isValidOutput(OutputStream output) {
+		if (output == null) {
+			return false;
+		} else {
+			try {
+				output.flush();
+			} catch (IOException ioException) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	boolean isValidMetadata(PoijoiMetaData metadata) {
+		if (metadata == null) {
+			return false;
+		} else {
+			if (metadata.getTableDefinitions() == null || metadata.getTableDefinitions().isEmpty()) {
+				return false;
+			} else if (metadata.isReadData() && metadata.getTableData() == null) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	
+}
---
+++ core/src/test/java/com/karlnosworthy/poijoi/io/writer/sql/SQLOutputStreamWriterTest.java
@@ -0,0 +1,154 @@
+package com.karlnosworthy.poijoi.io.writer.sql;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.LineNumberReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.karlnosworthy.poijoi.io.writer.Writer.WriteType;
+import com.karlnosworthy.poijoi.model.ColumnDefinition;
+import com.karlnosworthy.poijoi.model.ColumnDefinition.ColumnType;
+import com.karlnosworthy.poijoi.model.PoijoiMetaData;
+import com.karlnosworthy.poijoi.model.TableDefinition;
+
+public class SQLOutputStreamWriterTest {
+	
+	private SQLOutputStreamWriter outputStreamWriter;
+	
+	@Before
+	public void onSetup() {
+		outputStreamWriter = new SQLOutputStreamWriter();
+	}
+	
+	@After
+	public void onTeardown() {
+		outputStreamWriter = null;
+	}
+
+	/**
+	 * Check that passing in a null stream is handled safety.
+	 */
+	@Test
+	public void testWriteWithNullOutputStream() throws Exception {
+		assertFalse(outputStreamWriter.write(null, null, WriteType.BOTH));
+	}
+	
+	@Test
+	public void testWriteWithClosedInputStream() throws Exception {
+		
+		String javaTmpDir = System.getProperty("java.io.tmpdir");
+		File tempOutputFile = new File(javaTmpDir,"test.sql");
+		OutputStream outputStream = new FileOutputStream(tempOutputFile);
+		outputStream.close();
+		
+		assertFalse(outputStreamWriter.write(outputStream, null, WriteType.BOTH));
+	}
+	
+	@Test
+	public void testWriteWithNullMetadata() throws Exception {
+		String javaTmpDir = System.getProperty("java.io.tmpdir");
+		File testOutputFile = new File(javaTmpDir, "temp.sql");
+
+		OutputStream outputStream = null;
+		try {
+			outputStream = new FileOutputStream(testOutputFile);
+			assertFalse(outputStreamWriter.write(outputStream, null, WriteType.SCHEMA_ONLY));
+		} finally {
+			outputStream.close();
+		}
+	}
+	
+	@Test
+	public void testWriteWithInvalidMetadata() throws Exception {
+		String javaTmpDir = System.getProperty("java.io.tmpdir");
+		File testOutputFile = new File(javaTmpDir, "test.sql");
+
+		OutputStream outputStream = null;
+		try {
+			outputStream = new FileOutputStream(testOutputFile);
+			PoijoiMetaData metadata = new PoijoiMetaData(false, null, null);
+			assertFalse(outputStreamWriter.write(outputStream, metadata, WriteType.SCHEMA_ONLY));
+		} finally {
+			outputStream.close();
+		}
+	}
+	
+	/**
+	 * Based on a mocked up Table structure and data set make sure the writer
+	 * correctly outputs a valid ODS file
+	 */
+	@Test
+	public void testSuccessfulWrite() throws Exception {
+
+		Map<String, ColumnDefinition> columnDefinitions = new HashMap<String, ColumnDefinition>();
+		columnDefinitions.put("col1String", new ColumnDefinition("col1String",
+				0, ColumnType.STRING));
+		columnDefinitions.put("col2Date", new ColumnDefinition("col2Date", 1,
+				ColumnType.DATE));
+		columnDefinitions.put("col3Integer", new ColumnDefinition(
+				"col3Integer", 2, ColumnType.INTEGER_NUMBER));
+		columnDefinitions.put("col4Decimal", new ColumnDefinition(
+				"col4Decimal", 3, ColumnType.DECIMAL_NUMBER));
+
+		Map<String, TableDefinition> tableDefinitions = new HashMap<String, TableDefinition>();
+		tableDefinitions.put("TableOne", new TableDefinition("TableOne",
+				columnDefinitions));
+
+		HashMap<String, Object> row1Data = new HashMap<String, Object>();
+		row1Data.put("col1String", "hello");
+		Calendar cal = Calendar.getInstance();
+		cal.set(2014, 12, 31, 0, 0, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		row1Data.put("col2Date", cal.getTime());
+		row1Data.put("col3Integer", new Integer(19));
+		row1Data.put("col4Decimal", new Double("1.5"));
+
+		List<HashMap<String, Object>> rowData = new ArrayList<HashMap<String, Object>>();
+		rowData.add(row1Data);
+
+		Map<String, List<HashMap<String, Object>>> tableData = new HashMap<String, List<HashMap<String, Object>>>();
+		tableData.put("TableOne", rowData);
+
+		PoijoiMetaData metaData = new PoijoiMetaData(true, tableDefinitions,
+				tableData);
+		assertTrue(metaData.getTableData().size() == 1);
+		assertTrue(metaData.getTableDefinitions().size() == 1);
+		
+		String javaTmpDir = System.getProperty("java.io.tmpdir");
+		File testFile = new File(javaTmpDir, "temp.sql");
+		testFile.deleteOnExit();
+		
+		OutputStream outputStream = new FileOutputStream(testFile);
+		assertTrue(outputStreamWriter.write(outputStream, metaData, WriteType.BOTH));
+		outputStream.close();
+
+		LineNumberReader lineReader = new LineNumberReader(new FileReader(testFile));
+		
+		List<String> sqlLines = new ArrayList<String>();
+		String line = lineReader.readLine();
+		while (line != null && !line.isEmpty()) {
+			sqlLines.add(line);
+			line = lineReader.readLine();
+		}
+		lineReader.close();
+		
+		System.out.println("Lines = "+sqlLines.size());
+		
+		assertTrue(sqlLines.size() == 2);
+		assertTrue(sqlLines.get(0).equals("CREATE TABLE TableOne (id INTEGER PRIMARY KEY AUTOINCREMENT,\"col1String\" TEXT,\"col2Date\" DATE,\"col3Integer\" INTEGER,\"col4Decimal\" REAL);"));
+		assertTrue(sqlLines.get(1).equals("INSERT INTO 'TableOne' (\"col1String\",\"col2Date\",\"col3Integer\",\"col4Decimal\") VALUES ('hello','2015-01-31 00:00:00.000',19,1.5);"));
+	}
+}
